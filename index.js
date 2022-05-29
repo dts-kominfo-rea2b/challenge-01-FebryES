d925783cdec7cc6d3d748d269459787c4861fb5f
FebryES <febry.ermansyah86@gmail.com>
2022-05-29 19:09:35 +0700

    Menambahkan tugas Data Structure

--- index.js
+++ index.js
@@ -1,11 +1,125 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const FirstUser = {};
+const SecondUser = {};
+let convertBoolean=(pros)=> {
+    let answer
+    if (`props` == true) {
+        answer = "yes"
+    }
+    else {
+        answer = "No"
+    }
+    return answer
+}
+
+const favoriteColorFirstUser = [];
+favoriteColorFirstUser.push("Red")
+favoriteColorFirstUser.push("Blue")
+favoriteColorFirstUser.push("Black")
+favoriteColorFirstUser.push("Jingga")
+
+const sdFirstUser = {
+    name: "SD 01",
+    city: "Jakarta",
+    graduate: 2016
+}
+const smpFirstUser = {
+    name : "SMP 02",
+    city : "Jakarta",
+    graduate : 2019
+}
+const smaFirstUser = {
+    Name : "SMA 03",
+    graduate : "Tanggerang"
+}
+
+const educationFirstUser = [];
+educationFirstUser.push(sdFirstUser)
+educationFirstUser.push(smpFirstUser)
+educationFirstUser.push(smaFirstUser)
+
+const restauranSet = new Set ()
+restauranSet.add("Ramen")
+restauranSet.add("Tiramisu")
+restauranSet.add("Pangsit")
+restauranSet.add("Bajigur")
+restauranSet.add("Tektek")
+restauranSet.add("KueTiaw")
+restauranSet.add("Bihun")
+restauranSet.add("Rengginang")
+
+
+const firstUser = {
+    name : "Monic",
+    gender : "Female",
+    age : 17,
+    email : "monic@dingdong.com",
+    favoriteColor : favoriteColorFirstUser,
+    isHavetPet : convertBoolean(true),
+    education : JSON.stringify(educationFirstUser),
+    favoriterestauran : restauranSet,
+};
+
+const favoriteColorSecondUser = [];
+favoriteColorSecondUser.push("dark")
+favoriteColorSecondUser.push("Yelow")
+favoriteColorSecondUser.push("White")
+
+const sdSecoundUser ={
+    name : "SD 02",
+    city : "Jakarta",
+    graduate : 2010
+}
+
+const smpSecoundUser = {
+    name: "SMP 03",
+    city: "Bogor",
+    graduate: 2013
+}
+
+const smaSecoundUser = {
+    name: "SMA 01",
+    city: "Surabaya",
+    graduate: 2016
+}
+
+const univSecoundUser = {
+    name: "Universitas Mundur",
+    city: "Tanggerang"
+}
+
+const educationSecoundUser = [];
+educationSecoundUser.push(sdSecoundUser)
+educationSecoundUser.push(smpSecoundUser)
+educationSecoundUser.push(smaSecoundUser)
+educationSecoundUser.push(univSecoundUser)
+
+const restauranSecondUser = new Set()
+restauranSecondUser.add("Tempe")
+restauranSecondUser.add("Mendoan")
+restauranSecondUser.add("KueRangi")
+restauranSecondUser.add("NasiUduk")
+restauranSecondUser.add("SemurJengkol")
+restauranSecondUser.add("NasiKucing")
+restauranSecondUser.add("CangCimen")
+
+const secondUser = {
+    name: "wendy",
+    gender: "Male",
+    age: 23,
+    emai: "wendy@dingdong.com",
+    favoritColor: favoriteColorSecondUser,
+    isHavetPet: convertBoolean(false),
+    education: JSON.stringify(educationSecoundUser),
+    favoriterestauran: restauranSecondUser,
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser)
+users.push(secondUser)
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
